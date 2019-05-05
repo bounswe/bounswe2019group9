8b09eff2fe0dc11245d94c15912e27678ce460d8
corupta <halitozsoy1584@gmail.com>
2019-05-05 16:29:16 +0300

    fix script's settings module

--- practice-app/scripts/start_application.sh
+++ practice-app/scripts/start_application.sh
@@ -2,8 +2,8 @@
 cd /home/ec2-user/bounsweAss7API
 echo "Starting Application Practice App"
 (source ./venv/bin/activate &&
-python manage.py migrate &&
-python manage.py collectstatic &&
+python manage.py migrate --settings=practice_app.prod_settings &&
+python manage.py collectstatic --settings=practice_app.prod_settings &&
 uwsgi uwsgi-conf.ini &&
 echo "Successfully started Practice App") ||
 (>&2 echo "Failed to start Practice App")
