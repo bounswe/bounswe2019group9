d4d0664418a8bbafc1553b9422ddadf12c461b78
corupta <halitozsoy1584@gmail.com>
2019-05-05 16:21:59 +0300

    update scripts

--- practice-app/scripts/install_python_dependencies.sh
+++ practice-app/scripts/install_python_dependencies.sh
@@ -2,8 +2,10 @@
 cd /home/ec2-user/bounsweAss7API
 echo "Installing Dependencies for Practice App"
 (mkdir -p venv &&
-virtualenv -p /usr/bin/python3 venv/python3 &&
-source ./venv/python3/bin/activate &&
-pip3 install -r prod_requirements.txt &&
+virtualenv venv -p python3 &&
+source venv/bin/activate &&
+pip3 install -U pip &&
+PYTHON_INSTALL_LAYOUT="" pip3 install -r requirements.txt &&
+PYTHON_INSTALL_LAYOUT="" pip3 install -r prod_requirements.txt &&
 echo "Successfully Installed Dependencies") ||
 (>&2 echo "Failed to install Dependencies for Practice App")
--- practice-app/scripts/start_application.sh
+++ practice-app/scripts/start_application.sh
@@ -2,6 +2,8 @@
 cd /home/ec2-user/bounsweAss7API
 echo "Starting Application Practice App"
 (source ./venv/python3/bin/activate &&
+python manage.py migrate &&
+python manage.py collectstatic &&
 uwsgi uwsgi-conf.ini &&
 echo "Successfully started Practice App") ||
 (>&2 echo "Failed to start Practice App")
