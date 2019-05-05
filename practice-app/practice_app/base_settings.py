98590018c69f27f1671feec3155b666916b4621a
corupta <halitozsoy1584@gmail.com>
2019-05-05 23:56:11 +0300

    Revert "forgot to push secret mechanism"
    This reverts commit 5284637347b069425f3e080cec52c44ce90ec099.

--- practice-app/practice_app/base_settings.py
+++ practice-app/practice_app/base_settings.py
@@ -45,8 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls.apps.PollsConfig',
-    'translate.apps.TranslateConfig'
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
