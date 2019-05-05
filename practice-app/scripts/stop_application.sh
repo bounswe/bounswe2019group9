a23f5cf2c5ecf97c204912a7fd2f0b4c91afa9ba
corupta <halitozsoy1584@gmail.com>
2019-05-05 16:04:00 +0300

    upgrade stop script to handle when no running app

--- practice-app/scripts/stop_application.sh
+++ practice-app/scripts/stop_application.sh
@@ -2,6 +2,6 @@
 cd /home/ec2-user/bounsweAss7API
 echo "Stopping Practice App"
 (source ./venv/python3/bin/activate &&
-uwsgi --stop practice_app.uwsgi.pid &&
-echo "Successfully stopped Practice App") ||
-(>&2 "Failed to stop Practice App")
+    ([[ ! -f practice_app.uwsgi.pid ]] && echo "Practice App Wasn't Running, Skipping Stop") ||
+    (uwsgi --stop practice_app.uwsgi.pid && rm practice_app.uwsgi.pid && echo "Successfully stopped Practice App")
+) || (>&2 "Failed to stop Practice App")
