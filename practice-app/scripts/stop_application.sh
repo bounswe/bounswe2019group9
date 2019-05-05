5a091eb7d0811db658a9af105678e6d5fb364800
corupta <halitozsoy1584@gmail.com>
2019-05-05 20:13:39 +0300

    change travis to master only, modify stop application for when a pid file exists but the app is not running

--- practice-app/scripts/stop_application.sh
+++ practice-app/scripts/stop_application.sh
@@ -3,5 +3,8 @@ cd /home/ec2-user/bounsweAss7API
 echo "Stopping Practice App"
 (source ./venv/bin/activate &&
     ([[ ! -f practice_app.uwsgi.pid ]] && echo "Practice App Wasn't Running, Skipping Stop") ||
-    (uwsgi --stop practice_app.uwsgi.pid && rm practice_app.uwsgi.pid && echo "Successfully stopped Practice App")
+    (
+      (uwsgi --stop practice_app.uwsgi.pid && echo "Successfully stopped Practice App") ||
+      (echo "Practice App was killed already for some other reason")
+    ) && rm practice_app.uwsgi.pid
 ) || (>&2 echo "Failed to stop Practice App")
