b8bb3c5a84a7719515c4f7f9c8c3bd79576ef918
Irem Uguz <i_uguz@hotmail.com>
2019-05-09 22:24:11 +0300

    This was a temporary file and I forgot to delete it.
    I created this file while implementing the API, I forgot to delete it when I am done with it. I am deleting it now.

--- practice-app/obj/irem.py
+++
@@ -1,9 +0,0 @@
-from ibm_watson import VisualRecognitionV3
-from urllib.parse import urlencode
-import json
-
-
-visual_recognition = VisualRecognitionV3('2018-03-19',iam_apikey = 'wNhX6Wogs-1OgmHY_5efLHWC2Abv-dZVJ6rogx5KuqlB')
-picture_url = 'https://watson-developer-cloud.github.io/doc-tutorial-downloads/visual-recognition/640px-IBM_VGA_90X8941_on_PS55.jpg'
-classes_result = visual_recognition.classify(url=urlencode(picture_url)).get_result()
-print(json.dumps(classes_result,indent = 2))
