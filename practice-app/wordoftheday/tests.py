83e3c421d5d754b81d422f282242e353fff5ea94
afgedemenli <afgedemenli@gmail.com>
2019-05-10 13:16:56 +0300

    response status test created

--- practice-app/wordoftheday/tests.py
+++ practice-app/wordoftheday/tests.py
@@ -5,6 +5,29 @@ from django.urls import reverse
 
 class SimpleTest(TestCase):
 
+    def test_response_status(self):
+        request_en = RequestFactory().get('/en')
+        request_tr = RequestFactory().get('/tr')
+        request_aq = RequestFactory().get('/aq')
+        self.assertEqual(index(request_en, 'en').status_code, 200)
+        self.assertEqual(index(request_tr, 'tr').status_code, 200)
+        self.assertEqual(index(request_aq, 'aq').status_code, 200)
+
+
+    # I commented these tests since it would be a problem for us and will not be a useful test;
+    # because the word of the day will change everyday.
+
+'''
+    def test_English(self):
+        response = self.client.get(reverse("wordoftheday:index", args=("en",)))
+        word_returned = response.json()['word']
+        definition_returned = response.json()['definition']
+        request = RequestFactory().get('/en')
+        return_value = index(request, 'en')
+        self.assertEqual(return_value.status_code, 200)
+        self.assertEqual(word_returned, 'Purple')
+        self.assertEqual(definition_returned, 'A colour intermediate between red and blue.')
+
     def test_Turkish(self):
         response = self.client.get(reverse("wordoftheday:index", args=("tr",)))
         word_returned = response.json()['word']
@@ -15,8 +38,4 @@ class SimpleTest(TestCase):
         self.assertEqual(word_returned, 'Kırmızıbiber')
         self.assertEqual(definition_returned, 'Patlıcangillerden bir tür biber.')
 
-
-'''
-    def test_something_that_will_pass(self):
-        self.assertFalse(False)
 '''
