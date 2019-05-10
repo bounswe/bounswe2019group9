ec49391d4bcbafe1695d8831da3354723e6d4be5
afgedemenli <afgedemenli@gmail.com>
2019-05-10 09:24:06 +0300

    test added

--- practice-app/wordoftheday/tests.py
+++ practice-app/wordoftheday/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client, RequestFactory
+from .views import index
+from django.urls import reverse
 
-# Create your tests here.
+
+class SimpleTest(TestCase):
+
+    def test_Turkish(self):
+        response = self.client.get(reverse("wordoftheday:index", args=("tr",)))
+        word_returned = response.json()['word']
+        definition_returned = response.json()['definition']
+        request = RequestFactory().get('/tr')
+        return_value = index(request, 'tr')
+        self.assertEqual(return_value.status_code, 200)
+        self.assertEqual(word_returned, 'Kırmızıbiber')
+        self.assertEqual(definition_returned, 'Patlıcangillerden bir tür biber.')
+
+
+'''
+    def test_something_that_will_pass(self):
+        self.assertFalse(False)
+'''
