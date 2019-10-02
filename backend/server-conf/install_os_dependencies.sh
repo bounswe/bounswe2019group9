868688d74549be7bfc94a16c81ab95a47d92cbdd
Halit Özsoy <halitozsoy1584@gmail.com>
2019-10-02 20:44:05 +0300

    create scripts, update travis, not finished yet

--- backend/server-conf/install_os_dependencies.sh
+++ backend/server-conf/install_os_dependencies.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 sudo yum groupinstall -y "Development Tools"
 sudo yum install -y gcc openssl-devel bzip2-devel wget
-sudo yum install -y nginx
-sudo yum -y install mariadb-server
-sudo service mariadb start
-sudo service nginx start
-sudo yum install -y git python3
-sudo yum install -y python3-devel
-sudo pip3 install uwsgi
+sudo amazon-linux-extras install -y nginx1.12
+sudo yum install -y postgresql-server postgresql-contrib
+sudo postgresql-setup initdb
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
+sudo systemctl start nginx
+sudo systemctl enable nginx
 
 # this file is missing most of the configuration required to start a new server, the current server is mostly hand-configured instead, sadly
