03e318239e30fe906e7d1e02f170b89c989f8818
Halit Özsoy <halitozsoy1584@gmail.com>
2019-10-02 21:10:11 +0300

    update server confs

--- backend/server-conf/fix_permissions.sh
+++ backend/server-conf/fix_permissions.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/bounswe-backend
 sudo chown -R ec2-user:ec2-user .
+chmod 500 /home/ec2-user/bounswe-backend/language-learning-platform.jar
--- backend/server-conf/start_application.sh
+++ backend/server-conf/start_application.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/bounswe-backend
 echo "Starting Application Bounswe Backend"
-(service spring-boot-bounswe-backend start &&
+(sudo service spring-boot-bounswe-backend start &&
 echo "Successfully started Bounswe Backend") ||
 (>&2 echo "Failed to start Bounswe Backend")
