5a7e1e96985e24b6690bf0a2fbbde96138e9513e
Halit Özsoy <halitozsoy1584@gmail.com>
2019-10-03 03:46:13 +0300

    Revert "useless" to prevent tests from failing?
    This reverts commit 66ab5a410cecc4482076c6db653c865d4d77d5e1.

--- backend/src/main/java/hello/GreetingController.java
+++ backend/src/main/java/hello/GreetingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!!!";
+    private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
     @RequestMapping("/greeting")
