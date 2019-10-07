acdac8ccb1c704e49084fc1b1de042f07ca38df7
Gamze Gülbahar <g.gulbahar17@gmail.com>
2019-10-07 21:21:32 +0300

    the response was taken as JSONObject and parsed so that only content field was printed

--- android/app/src/main/java/com/example/learningplatform/DisplayMessage.java
+++ android/app/src/main/java/com/example/learningplatform/DisplayMessage.java
@@ -10,8 +10,12 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class DisplayMessage extends AppCompatActivity {
 
@@ -29,24 +33,47 @@ public class DisplayMessage extends AppCompatActivity {
 
         Intent intent = getIntent();
         String person = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-
         String url ="https://api.bounswe2019group9.tk/greeting?name="+ person ;
 
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+        //String url2 ="http://api.openweathermap.org/data/2.5/weather?id=745044&APPID=ea1a1745f60c4b98c789f39c611dcf19&lang=tr";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+
+                        //textView.setText("Response: " + response.toString());
+                        try {
+
+                            String responseString = (String) response.get("content");
+
+                            //JSONObject main = (JSONObject) response.get("main");      this code block for getting
+                            //Double temp = (Double) main.get("temp");                  the json data inside a jsonObject
+                            //String responseString = String.valueOf(temp);
+
+                            textView.setText(responseString);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+/*      StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
+
                         textView.setText("Response is: "+ response);
                     }
-                }, new Response.ErrorListener() {
+                }*/, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
+                textView.setText(error.toString());
+                System.out.println("That didn't work!");
             }
         });
 
         // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+        queue.add(jsonObjectRequest);
     }
 }
