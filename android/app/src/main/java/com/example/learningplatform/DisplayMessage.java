cb8c288a17bfeef7d9064088599ad1fdc4709ea2
ibrahimkaplan07 <ibrahimckaplan@gmail.com>
2019-10-08 00:56:00 +0300

    Redesigned login screen

--- android/app/src/main/java/com/example/learningplatform/DisplayMessage.java
+++ android/app/src/main/java/com/example/learningplatform/DisplayMessage.java
@@ -2,7 +2,6 @@ package com.example.learningplatform;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
@@ -11,7 +10,6 @@ import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
@@ -22,8 +20,7 @@ public class DisplayMessage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
-
+        setContentView(R.layout.activity_welcome);
 
 
         final TextView textView;
@@ -31,9 +28,7 @@ public class DisplayMessage extends AppCompatActivity {
 
         RequestQueue queue = Volley.newRequestQueue(this);
 
-        Intent intent = getIntent();
-        String person = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
-        String url ="https://api.bounswe2019group9.tk/greeting?name="+ person ;
+        String url ="https://api.bounswe2019group9.tk/greeting?name=" + "Group9" ;
 
         //String url2 ="http://api.openweathermap.org/data/2.5/weather?id=745044&APPID=ea1a1745f60c4b98c789f39c611dcf19&lang=tr";
 
@@ -42,7 +37,6 @@ public class DisplayMessage extends AppCompatActivity {
                     @Override
                     public void onResponse(JSONObject response) {
 
-                        //textView.setText("Response: " + response.toString());
                         try {
 
                             String responseString = (String) response.get("content");
@@ -58,22 +52,15 @@ public class DisplayMessage extends AppCompatActivity {
 
                     }
                 }
-/*      StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-
-                        textView.setText("Response is: "+ response);
-                    }
-                }*/, new Response.ErrorListener() {
+                , new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 textView.setText(error.toString());
-                System.out.println("That didn't work!");
+                //System.out.println("That didn't work!");
             }
         });
 
-        // Add the request to the RequestQueue.
         queue.add(jsonObjectRequest);
+
     }
 }
