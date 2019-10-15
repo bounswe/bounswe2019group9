8b9d9e501cac0ad6832cca34071cb654da8b3720
irem-uguz <i_uguz@hotmail.com>
2019-10-15 18:40:47 +0300

    Bug fix about login-resiter butons

--- android/app/src/main/java/com/example/learningplatform/DisplayMessage.java
+++ android/app/src/main/java/com/example/learningplatform/DisplayMessage.java
@@ -26,7 +26,6 @@ public class DisplayMessage extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_welcome);
         setContentView(R.layout.language_selection);
         recyclerView = findViewById(R.id.recylerview);
 
@@ -36,39 +35,6 @@ public class DisplayMessage extends AppCompatActivity {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(linearLayoutManager);
-
-        /*final TextView textView;
-        textView = findViewById(R.id.textView);
-
-        RequestQueue queue = Volley.newRequestQueue(this);
-
-        String url ="https://api.bounswe2019group9.tk/greeting?name=" + "Group9" ;
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-
-                        try {
-
-                            String responseString = (String) response.get("content");
-
-                            textView.setText(responseString);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-                    }
-                }
-                , new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                textView.setText(error.toString());
-
-            }
-        });
-
-        queue.add(jsonObjectRequest);*/
 
     }
 }
--- android/app/src/main/java/com/example/learningplatform/MainActivity.java
+++ android/app/src/main/java/com/example/learningplatform/MainActivity.java
@@ -22,8 +22,6 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
     private static EditText username;
     private static EditText password;
-    private static Button login_button;
-    private static Button register_button;
 
 
     @Override
@@ -31,86 +29,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void buttonClick(View view) {
-        switch (view.getId()) {
-            case R.id.login_button:
-                LoginButton();
-                break;
-            case R.id.register_now_button:
-                RegisterButton();
-                break;
-        }
-    }
 
 
-    public void LoginButton(){
+
+    public void LoginButton(final View v){
 
         username = findViewById(R.id.login_email);
         password = findViewById(R.id.login_password);
-        login_button = findViewById(R.id.login_button);
 
+        JSONObject login_data = new JSONObject();
+        try {
+            login_data.put("email",username.getText().toString());
+            login_data.put("password",password.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
 
-
-        login_button.setOnClickListener(
-                new View.OnClickListener() {
+        RequestQueue queue = Volley.newRequestQueue(v.getContext());
+        String url = "https://api.bounswe2019group9.tk/users/login";
+        JsonObjectRequest loginJsonReq = new JsonObjectRequest(Request.Method.POST, url,login_data,
+                new Response.Listener<JSONObject>() {
                     @Override
-                    public void onClick(final View v) {
-
-                            JSONObject login_data = new JSONObject();
-                            try {
-                                login_data.put("email",username.getText().toString());
-                                login_data.put("password",password.getText().toString());
-                            } catch (JSONException e) {
-                                e.printStackTrace();
+                    public void onResponse(JSONObject response) {
+                        try {
+                            int statusCode =  response.getInt("status");
+                            if(statusCode == 200){
+                                Toast.makeText(MainActivity.this,"Username and password is correct",
+                                        Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(v.getContext(),DisplayMessage.class);
+                                v.getContext().startActivity(intent);
                             }
+                            else{
+                                Toast.makeText(MainActivity.this,"Username and password is NOT correct",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
-                            RequestQueue queue = Volley.newRequestQueue(v.getContext());
-                            String url = "https://api.bounswe2019group9.tk/users/login";
-                            JsonObjectRequest loginJsonReq = new JsonObjectRequest(Request.Method.POST, url,login_data,
-                                    new Response.Listener<JSONObject>() {
-                                        @Override
-                                        public void onResponse(JSONObject response) {
-                                            try {
-                                                int statusCode =  response.getInt("status");
-                                                if(statusCode == 200){
-                                                    Toast.makeText(MainActivity.this,"Username and password is correct",
-                                                            Toast.LENGTH_SHORT).show();
-                                                    Intent intent = new Intent(v.getContext(),DisplayMessage.class);
-                                                    v.getContext().startActivity(intent);
-                                                }
-                                                else{
-                                                    Toast.makeText(MainActivity.this,"Username and password is NOT correct",
-                                                            Toast.LENGTH_SHORT).show();
-                                                }
-                                            } catch (JSONException e) {
-                                                e.printStackTrace();
-                                            }
-
-                                        }
-                                    },
-                                    new Response.ErrorListener() {
-                                        @Override
-                                        public void onErrorResponse(VolleyError error) {
-                                            System.out.println("That didn't work!");
-                                        }
-                                    });
-
-                        queue.add(loginJsonReq);
                     }
-                }
-        );
-           }
-    public void RegisterButton(){
-        register_button = findViewById(R.id.register_now_button);
-        register_button.setOnClickListener(
-                new View.OnClickListener() {
+                },
+                new Response.ErrorListener() {
                     @Override
-                    public void onClick(View v) {
-                        Intent registerIntent = new Intent(v.getContext(),RegisterActivity.class);
-                        v.getContext().startActivity(registerIntent);
+                    public void onErrorResponse(VolleyError error) {
+                        System.out.println("That didn't work!");
                     }
-                }
-        );
+                });
+
+        queue.add(loginJsonReq);
+    }
+    public void RegisterButton(View v){
+        Intent registerIntent = new Intent(v.getContext(),RegisterActivity.class);
+        v.getContext().startActivity(registerIntent);
 
     }
 
