86a82d174a9b7acac8b781c6bbb6f256b738b316
afgedemenli <afgedemenli@gmail.com>
2019-10-22 13:42:25 +0300

    Fix grade get query

--- backend/src/main/java/app/actor/repository/GradeRepository.java
+++ backend/src/main/java/app/actor/repository/GradeRepository.java
@@ -1,6 +1,5 @@
 package app.actor.repository;
 
-import app.Response;
 import app.actor.entity.Grade;
 import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -21,6 +20,6 @@ public interface GradeRepository extends JpaRepository<Grade, Long> {
   @Query(value = "INSERT INTO grades(user_id, language_id, grade) values(:userId, :languageId, :grade)", nativeQuery = true)
   void addGrade(@Param("userId") Long userId, @Param("languageId") Integer languageId, @Param("grade") Integer grade);
 
-  @Query(value = "SELECT * FROM grades WHERE user_id=:userId AND language_id=:languageId", nativeQuery = true)
+  @Query(value = "SELECT * FROM grades WHERE user_id=:userId AND language_id=:languageId LIMIT 1", nativeQuery = true)
   Grade getGradeByUserIdAndLanguageId(@Param("userId") Long userId, @Param("languageId") Integer languageId);
 }
