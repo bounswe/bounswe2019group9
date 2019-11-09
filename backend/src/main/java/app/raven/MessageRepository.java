36c0e44e6479c01a6f311f7e233dcbad3f0177b3
afgedemenli <afgedemenli@gmail.com>
2019-11-09 19:08:24 +0300

    Minor bug fix: Add extends to message repository

--- backend/src/main/java/app/raven/MessageRepository.java
+++ backend/src/main/java/app/raven/MessageRepository.java
@@ -3,13 +3,14 @@ package app.raven;
 import java.util.Date;
 import java.util.List;
 import javax.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MessageRepository {
+public interface MessageRepository extends JpaRepository<Message, Long> {
 
   @Transactional
   @Modifying
