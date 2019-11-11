d7630444a04ce1c314fafa3ced0cb16fb0c6baf5
irem-uguz <i_uguz@hotmail.com>
2019-11-11 22:18:51 +0300

    added bottom navigation bar, only profile page works

--- android/app/src/main/java/com/example/learningplatform/LanguageListDisplay.java
+++ android/app/src/main/java/com/example/learningplatform/LanguageListDisplay.java
@@ -1,9 +1,14 @@
 package com.example.learningplatform;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class LanguageListDisplay extends AppCompatActivity {
     RecyclerView recyclerView;
@@ -15,6 +20,26 @@ public class LanguageListDisplay extends AppCompatActivity {
         recyclerView = findViewById(R.id.recylerview);
         LanguageAdapter languageAdapter = new LanguageAdapter(this);
         recyclerView.setAdapter(languageAdapter);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_bar_excercise:
+                        return true;
+                    case R.id.nav_bar_message:
+                        return true;
+                    case R.id.nav_bar_profile:
+                        Intent intent = new Intent(LanguageListDisplay.this, ProfilePageActivity.class);
+                        startActivity(intent);
+                        return true;
+                    case R.id.nav_bar_search:
+                        return true;
+                }
+                return true;
+            }
+        });
     }
 
 }
