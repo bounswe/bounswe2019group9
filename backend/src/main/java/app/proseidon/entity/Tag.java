5d5cd561429d82c9f7f92350471c48b196fc6412
afgedemenli <afgedemenli@gmail.com>
2019-11-13 14:35:55 +0300

    Create tag entity

---
+++ backend/src/main/java/app/proseidon/entity/Tag.java
@@ -0,0 +1,62 @@
+package app.proseidon.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author ahmet.gedemenli
+ */
+
+@Entity
+@Table(name = "tags")
+public class Tag {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  private Long exerciseId;
+
+  private String tagText;
+
+  public Tag(Long exerciseId, String tagText) {
+    this.exerciseId = exerciseId;
+    this.tagText = tagText;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getExerciseId() {
+    return exerciseId;
+  }
+
+  public void setExerciseId(Long exerciseId) {
+    this.exerciseId = exerciseId;
+  }
+
+  public String getTagText() {
+    return tagText;
+  }
+
+  public void setTagText(String tagText) {
+    this.tagText = tagText;
+  }
+
+  @Override
+  public String toString() {
+    return "Tag{" +
+           "id=" + id +
+           ", exerciseId=" + exerciseId +
+           ", tagText='" + tagText + '\'' +
+           '}';
+  }
+}
