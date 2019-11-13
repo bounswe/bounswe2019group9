5b34d40ac64c1497e65d2dbd3db5182d6af46bf0
afgedemenli <afgedemenli@gmail.com>
2019-11-13 12:56:39 +0300

    Implement service for getting user profiles of conversations sorted chronologically.

--- backend/src/main/java/app/raven/service/ConversationService.java
+++ backend/src/main/java/app/raven/service/ConversationService.java
@@ -1,9 +1,12 @@
 package app.raven.service;
 
+import app.actor.bean.ProfileInfo;
+import app.actor.service.ProfileService;
 import app.common.HttpResponses;
 import app.common.Response;
 import app.raven.entity.Conversation;
 import app.raven.repository.ConversationRepository;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
@@ -14,13 +17,31 @@ import org.springframework.stereotype.Service;
 @Service
 public class ConversationService {
 
+  private final ProfileService profileService;
+
   private final ConversationRepository conversationRepository;
 
-  public ConversationService(ConversationRepository conversationRepository) {
+  public ConversationService(ProfileService profileService,
+                             ConversationRepository conversationRepository) {
+    this.profileService = profileService;
     this.conversationRepository = conversationRepository;
   }
 
-  public Response<List<Conversation>> getConversationsByUserId(Long userId){
-    return HttpResponses.from(conversationRepository.getConvesationsByUserId(userId));
+  public List<Conversation> getConversationsByUserId(Long userId) {
+    return conversationRepository.getConvesationsByUserId(userId);
+  }
+
+  public Response<List<ProfileInfo>> getConversationProfileInfosByUserId(Long userId) {
+    List<Conversation> conversations = getConversationsByUserId(userId);
+    ArrayList<ProfileInfo> responseBody = new ArrayList<>();
+    for (Conversation conversation : conversations) {
+      responseBody.add(getProfileInfoFromConversation(conversation, userId));
+    }
+    return HttpResponses.from(responseBody);
+  }
+
+  private ProfileInfo getProfileInfoFromConversation(Conversation conversation, Long userId) {
+    return profileService.getProfileInfoByUserId(
+        conversation.getUserIdOne() == userId ? conversation.getUserIdTwo() : conversation.getUserIdOne());
   }
 }
