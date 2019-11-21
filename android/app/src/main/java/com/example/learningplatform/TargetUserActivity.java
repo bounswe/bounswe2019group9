e1e07ebfe37a4272d4bce15e6bdd3814d807efb9
ibrahimkaplan07 <ibrahimckaplan@gmail.com>
2019-11-21 22:09:51 +0300

    added target user profile page which is reachable from user search

---
+++ android/app/src/main/java/com/example/learningplatform/TargetUserActivity.java
@@ -0,0 +1,14 @@
+package com.example.learningplatform;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TargetUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_target_user);
+    }
+}
