af9c7a4c5b8f96e98c71ecf31cfea962191d50ee
afgedemenli <afgedemenli@gmail.com>
2019-11-23 21:55:26 +0300

    Create exercise search controller and search request

--- backend/src/main/java/app/cerebro/controller/SearchController.java
+++ backend/src/main/java/app/cerebro/controller/SearchController.java
@@ -1,9 +1,11 @@
 package app.cerebro.controller;
 
 import app.actor.bean.ProfileInfo;
+import app.cerebro.bean.SearchExerciseRequest;
 import app.cerebro.service.SearchOperatorService;
 import app.cerebro.bean.SearchUserRequest;
 import app.common.Response;
+import app.proseidon.entity.Exercise;
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,5 +29,10 @@ public class SearchController {
   @PostMapping("/users")
   public Response<List<ProfileInfo>> searchUser(@RequestBody SearchUserRequest request) {
     return searchOperatorService.getSearchUserService().searchUser(request);
+  }
+
+  @PostMapping("/exercises")
+  public Response<List<Exercise>> searchExercise(@RequestBody SearchExerciseRequest request) {
+    return null;
   }
 }
