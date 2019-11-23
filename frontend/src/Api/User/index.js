b0a1ee6895530bccabb163911d2c9cefef41e374
Egemen Göl <42511947+egemengol@users.noreply.github.com>
2019-11-23 19:34:30 +0300

    Created API binding

--- frontend/src/Api/User/index.js
+++ frontend/src/Api/User/index.js
@@ -18,6 +18,31 @@ import {getRequest, postRequest} from '../base';
 export const getUserById = (userId, extras) => getRequest('users/get', { id: userId }, extras);
 
 /**
+ * @function getProfileInfoByUserId
+ * @param userId
+ * @param extras object?
+ * @return {Promise<AxiosResponse<T>>}
+ * Success Response:
+ * {
+ *  "email": "string",
+ *  "firstName": "string",
+ *  "grades": [
+ *    0
+ *  ],
+ *  "languages": [
+ *    "string"
+ *  ],
+ *  "lastName": "string",
+ *  "progressLevels": [
+ *    0
+ *  ],
+ *  "userId": 0
+ * }
+ *
+ */
+export const getProfileInfoByUserId = (userId, extras) => getRequest('users/profile', { id: userId }, extras);
+
+/**
  * @function login
  * @param data
  * @param extras object?
