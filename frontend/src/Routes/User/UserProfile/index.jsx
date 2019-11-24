b7ba39c13712c20d4440ca55fb0244800d4286f6
Egemen Göl <42511947+egemengol@users.noreply.github.com>
2019-11-24 23:40:50 +0300

    Visual polish done on profile page.

--- frontend/src/Routes/User/UserProfile/index.jsx
+++ frontend/src/Routes/User/UserProfile/index.jsx
@@ -1,33 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
 import { useParams } from "react-router-dom";
-import { Row, Col } from "antd";
+import { Card, Descriptions, Skeleton } from "antd";
 
 import { connect, storeType } from '../../../Store';
 import { getProfileInfoByUserId } from '../../../Api/User';
+import { numGradeToStrGrade } from '../../../Helpers/grades';
+import { CenterView } from '../../../Layouts/index';
+import "./index.css";
 
-
-const BoxedList = ({ title, keyValueList }) => {
-  const styles = {
-    key: {
-      fontWeight: "bold",
-    },
-    title: {
-      fontWeight: "bold",
-      fontSize: "%120"
-    }
-  }
-  return (
-  <Col>
-    <Row style={styles.title}>{title}</Row>
-    {keyValueList.map( ({key, value}) => (
-      <Row>
-        <Col span={10} style={styles.key}>{key}</Col>
-        <Col span={14}>{value}</Col>
-      </Row>
-    ))}
-  </Col>
-)};
 
 const UserProfile = ({ store: { userId } }) => {
   const params = useParams();
@@ -49,52 +29,34 @@ const UserProfile = ({ store: { userId } }) => {
 
   const { email, firstName, grades, languages, lastName, progressLevels } = profile || {};
 
-  const languagesBoxFriendly = (languages || []).map((lang, i) => (
+  const languagesTuple = (languages || []).map((lang, i) => (
     [lang, grades[i], progressLevels[i]]
-  )).map( tuple => ({
-    title: tuple[0],
-    keyValueList: [
-      {
-        key: "Grade",
-        value: tuple[1],
-      },{
-        key: "Progress Level",
-        value: tuple[2],
-      },
-    ],
-  }));
-
-  const basic = {
-    title: "Basic Info",
-    keyValueList: [
-      {
-        key: "Full Name",
-        value: [firstName, lastName].join(' '),
-      },
-      {
-        key: "Email",
-        value: email,
-      },
-    ]
-  };
+  ));
 
   return (
-    <>
-      <Row type="flex" justify="center" align="middle" span={8}>
-        <Col span={8}>
-          <BoxedList style={styles.BoxedList} title={basic.title} keyValueList={basic.keyValueList} />
-        </Col>
-        <Col span={8}>
-          {(languagesBoxFriendly || []).map( box => 
-            <BoxedList 
-              style={styles.BoxedList}
-              title={box.title} 
-              keyValueList={box.keyValueList} /> 
-          )}
-        </Col>
-      </Row>
-    </>
-  )
+    <CenterView>
+      <Card title="Profile Page" style={styles.card}>
+        <Skeleton loading={!profile} title={true} active>
+          <Descriptions title="User Info">
+            <Descriptions.Item label="Full Name" className={"capitalize-text"}>
+              {[firstName, lastName].join(' ')}
+            </Descriptions.Item>
+            <Descriptions.Item label="Email">{email}</Descriptions.Item>
+          </Descriptions>
+          { languagesTuple.length ? languagesTuple.map( tuple => 
+            <Descriptions title={tuple[0]}>
+              <Descriptions.Item label="Grade">
+                {numGradeToStrGrade(tuple[1])}
+              </Descriptions.Item>
+              <Descriptions.Item label="Progress Level">
+                {tuple[2]}
+              </Descriptions.Item>
+            </Descriptions>
+          ) : <p>No languages found.</p>}
+        </Skeleton>
+      </Card>
+    </CenterView>
+  );
 }
 
 UserProfile.propTypes = {
@@ -102,6 +64,9 @@ UserProfile.propTypes = {
 };
 
 const styles = {
+  card: {
+    maxWidth: 560,
+  }
 };
 
 export default connect(UserProfile);
