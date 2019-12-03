df58561133d6f11e836e8922d62ae4adf95c81b9
afgedemenli <afgedemenli@gmail.com>
2019-12-03 19:12:48 +0300

    Polish

--- backend/src/main/java/app/mahmuthoca/AmazonClient.java
+++ backend/src/main/java/app/mahmuthoca/AmazonClient.java
@@ -5,7 +5,6 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,7 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class AmazonClient {
 
-  public AmazonS3 s3client;
+  private AmazonS3 s3client;
 
   @Value("${amazonProperties.endpointUrl}")
   private String endpointUrl;
@@ -27,6 +26,10 @@ public class AmazonClient {
 
   @Value("${amazonProperties.secretKey}")
   private String secretKey;
+
+  public AmazonS3 getS3client() {
+    return this.s3client;
+  }
 
   @PostConstruct
   private void initializeAmazon() {
