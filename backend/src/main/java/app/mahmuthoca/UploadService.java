ee2bc2deb82817da3d90d165f2efecf18fd04e4d
afgedemenli <afgedemenli@gmail.com>
2019-12-03 20:49:53 +0300

    Add png and jpeg types for image upload

--- backend/src/main/java/app/mahmuthoca/UploadService.java
+++ backend/src/main/java/app/mahmuthoca/UploadService.java
@@ -16,6 +16,10 @@ public class UploadService {
 
   private static final String BUCKET_NAME = "kereviz-upload";
 
+  private static final String PNG_TYPE = "image/png";
+
+  private static final String JPEG_TYPE = "image/jpeg";
+
   private static final String IMAGE_FOLDER_URL = "https://kereviz-upload.s3.eu-central-1.amazonaws.com/image/";
 
   private final AmazonClient amazonClient;
@@ -25,23 +29,31 @@ public class UploadService {
   }
 
   public String uploadImage(FileUploadRequest request) {
+    String extension = "";
     try {
+      String base64Data = request.getBase64Data();
       byte[] bI =
-          Base64.decodeBase64(request.getBase64Data().substring(request.getBase64Data().indexOf(",") + 1).getBytes());
+          Base64.decodeBase64(base64Data.substring(base64Data.indexOf(",") + 1).getBytes());
       InputStream fis = new ByteArrayInputStream(bI);
+      String type = base64Data.substring(base64Data.indexOf(":") + 1, base64Data.indexOf(";"));
       ObjectMetadata metadata = new ObjectMetadata();
       metadata.setContentLength(bI.length);
-      metadata.setContentType("images/png");
+      if (type.equals(PNG_TYPE) || type.equals(JPEG_TYPE)) {
+        metadata.setContentType(type);
+      } else {
+        return null;
+      }
+      extension = ".".concat(type.substring(type.indexOf("/") + 1));
       metadata.setCacheControl("public, max-age=31536000");
       this.amazonClient.getS3client()
-                       .putObject(BUCKET_NAME, "image/".concat(request.getExerciseId().toString().concat(".png")), fis,
-                                  metadata);
+                       .putObject(BUCKET_NAME, "image/".concat(request.getExerciseId().toString().concat(extension)),
+                                  fis, metadata);
       this.amazonClient.getS3client()
-                       .setObjectAcl(BUCKET_NAME, "image/".concat(request.getExerciseId().toString().concat(".png")),
+                       .setObjectAcl(BUCKET_NAME, "image/".concat(request.getExerciseId().toString().concat(extension)),
                                      CannedAccessControlList.PublicRead);
     } catch (Exception e) {
       return null;
     }
-    return IMAGE_FOLDER_URL.concat(request.getExerciseId().toString()).concat(".png");
+    return IMAGE_FOLDER_URL.concat(request.getExerciseId().toString()).concat(extension);
   }
 }
