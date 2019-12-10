07641731336ede31cf6226cc1b46181b2029748b
afgedemenli <afgedemenli@gmail.com>
2019-12-10 19:09:29 +0300

    Update FileUploadRequest

--- backend/src/main/java/app/mahmuthoca/FileUploadRequest.java
+++ backend/src/main/java/app/mahmuthoca/FileUploadRequest.java
@@ -1,17 +1,26 @@
 package app.mahmuthoca;
 
+import javax.validation.constraints.NotNull;
+import org.springframework.lang.Nullable;
+
 /**
  * @author ahmet.gedemenli
  */
 
 public class FileUploadRequest {
 
+  @NotNull
   private Long exerciseId;
 
+  @Nullable
+  private Long authorId;
+
+  @NotNull
   private String base64Data;
 
-  public FileUploadRequest(Long exerciseId, String base64Data) {
+  public FileUploadRequest(Long exerciseId, Long authorId, String base64Data) {
     this.exerciseId = exerciseId;
+    this.authorId = authorId;
     this.base64Data = base64Data;
   }
 
@@ -21,6 +30,14 @@ public class FileUploadRequest {
 
   public void setExerciseId(Long exerciseId) {
     this.exerciseId = exerciseId;
+  }
+
+  public Long getAuthorId() {
+    return authorId;
+  }
+
+  public void setAuthorId(Long authorId) {
+    this.authorId = authorId;
   }
 
   public String getBase64Data() {
@@ -35,6 +52,7 @@ public class FileUploadRequest {
   public String toString() {
     return "FileUploadRequest{" +
            "exerciseId=" + exerciseId +
+           ", authorId=" + authorId +
            ", base64Data='" + base64Data + '\'' +
            '}';
   }
