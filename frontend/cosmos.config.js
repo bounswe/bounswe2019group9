b6ee0b35cbecd31a401e01f38b743b785c7e0542
corupta <corupta@MacBook-Pro-di-corupta.local>
2019-12-10 16:03:09 +0300

    Add react-cosmos

---
+++ frontend/cosmos.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    globalImports: [
+        'antd/dist/antd.css',
+        'react-toastify/dist/ReactToastify.min.css',
+        './src/index.css',
+    ],
+    publicPath: 'public',
+    publicUrl: '/',
+};
